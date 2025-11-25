8f5aba66520fddae166592d82fa6e932db25c941
975569220@qq.com <975569220@qq.com>
2025-11-25 23:45:23 +0800

    city list

---
+++ src/components/CityList.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/prop-types */
+import style from "./CityList.module.css";
+import CityItem from "./CityItem";
+import Spinner from "./Spinner";
+import Message from "./Message";
+
+function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map " />
+    );
+  return (
+    <ul className={style.cityList}>
+      {cities?.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
+}
+
+export default CityList;
