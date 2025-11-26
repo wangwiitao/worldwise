7c7b5a75e50ff698e7480bbfcabf711f2852bec9
975569220@qq.com <975569220@qq.com>
2025-11-26 23:29:51 +0800

    programmatic navigation with navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
@@ -43,7 +43,7 @@ function App() {
         <Route path="app" element={<AppLayout />}>
           <Route
             index
-            element={<CityList cities={cities} isLoading={isLoading} />}
+            element={<Navigate to='cities' replace/>}
           />
           <Route
             path="cities"
