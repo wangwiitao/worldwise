4ae0f3051d41d34d4eb3fa99cf3498e376d6112e
975569220@qq.com <975569220@qq.com>
2025-11-26 20:42:31 +0800

    implementing the country list

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import { useState, useEffect } from "react";
+import CountryList from "./components/CountryList";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -46,7 +47,10 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="countries" element={<p>Countries</p>} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />}></Route>
---
+++ src/components/CountryList.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+import style from "./CountryList.module.css";
+import CountryItem from "./CountryItem";
+import Spinner from "./Spinner";
+import Message from "./Message";
+
+function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map " />
+    );
+  const countries = cities.reduce((acc, cur) => {
+    if (!acc.map((item) => item.country).includes(cur.country)) {
+      return [...acc, { country: cur.country, emoji: cur.emoji }];
+    } else {
+      return acc;
+    }
+  }, []);
+  return (
+    <ul className={style.countryList}>
+      {countries?.map((country) => (
+        <CountryItem country={country} key={country.id} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
